fd1bb517dbeacabc19e47e7830d19992da86e5ab
avelreserch <pavel.oliynyk@gmail.com>
2019-07-01 13:21:14 +0100

    Initial commit

---
+++ example.hs
@@ -0,0 +1,19 @@
+processingString :: String -> Int
+
+processingString = undefined
+
+data  Compass = North | East | South | West
+  deriving (Eq, Ord, Enum, Show)
+
+data Expression = Number Int
+  | Add Expression Expression
+  | Sub Expression Expression
+  deriving (Eq, Ord, Show)
+
+calculate :: Expression -> Int
+calculate (Number x) = x
+calculate (Add x y) = (calculate x) + (calculate y)  
+calculate (Sub x y) = (calculate x) - (calculate y)
+
+newHead [] = error "empty list"
+newHead (x:xs) = x
---
+++ test.hs
@@ -0,0 +1,9 @@
+main :: IO()
+main = putStrLn(greet "World")
+
+greeting = "Data test"
+greet who = greeting ++ " " ++ who
+
+add :: Int -> Int -> Int
+add a b = a + b
+
